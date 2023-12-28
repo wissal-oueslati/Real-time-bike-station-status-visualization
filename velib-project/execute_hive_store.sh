1bbf56e800e401508b6e56617b6b6f5f12b5fd83
wissal-oueslati <118764145+wissal-oueslati@users.noreply.github.com>
2023-12-28 12:18:56 +0100

    Delete velib-project directory

--- velib-project/execute_hive_store.sh
+++
@@ -1,5 +0,0 @@
-spark-submit \
-  --master local[*]\
-  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.apache.spark:spark-hive_2.12:3.2.4 \
-  --conf "spark.jars.packages=org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.apache.spark:spark-hive_2.12:3.2.4" \
-  hive_store.py > out.txt
